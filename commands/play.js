4696d8c5bf99600183091b17664b1633bb524d02
Thomas Priftakis <tpriftakis@gmail.com>
2023-01-14 11:04:56 -0500

    play command

--- commands/play.js
+++ commands/play.js
@@ -3,8 +3,18 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("play")
-        .setDescription("Plays and audiobook"),
+        .setDescription("Plays and audiobook")
+        .addAttachmentOption((option) => option
+            .setName("mp3-file")
+            .setDescription("Upload a file to play")
+            .setRequired(true)),
+
     async execute(interaction) {
+        const attachment = interaction.options.getAttachment("mp3-file");
+        const name = attachment.url;
+
+        console.log(name);
+
 
         function getChannelName(channel){
             return channel.name;
